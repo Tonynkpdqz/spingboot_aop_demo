98b9c3e9b1632e49461b2f474817224067f3c94f
Nkpdqz <529446386@qq.com>
2020-04-09 11:22:30 +0800

    在springboot里使用aop，only for test

---
+++ src/main/java/com/nkpdqz/spingboot_aop_demo/SpingbootAopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nkpdqz.spingboot_aop_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootAopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootAopDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nkpdqz/spingboot_aop_demo/SpingbootAopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkpdqz.spingboot_aop_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootAopDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
